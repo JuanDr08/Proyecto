ed3fe22c47a71378428704373a2f6410ee95ad30
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-03 00:47:42 -0500

    feat: :sparkles: Camper Menu
    added the camper menu with the crud options to start saving data in the json

--- Modulos/Cruds/CrudCamp.py
+++ Modulos/Cruds/CrudCamp.py
@@ -1,3 +1,4 @@
+import os, json
 def create():
     pass
 def read():
@@ -7,4 +8,35 @@ def update():
 def delete():
     pass
 def menu():
-    pass
+    titulo = """
+     -----------------------------------
+    |   Bienvenido Al Menu De Camper    |
+     -----------------------------------
+"""
+    opciones = "1. Inscribir un camper\n2. Buscar un camper\n3. Modificar informacion de un camper\n4. Eliminar un camper\n5. Volver"
+    while True:
+        os.system("cls")
+        print(titulo)
+        print(opciones)
+        try:
+            opc = int(input("Ingrese la opcion que desea realizar ->"))
+            match(opc):
+                case 1:
+                    create()
+                case 2:
+                    read()
+                case 3:
+                    update()
+                case 4:
+                    delete()
+                case 5:
+                    os.system("cls")
+                    print("volviendo al menu principal...")
+                    os.system("pause")
+                    break
+                case _:
+                    print("- Ingrese una opcion de las disponibles")
+                    os.system("pause")
+        except ValueError:
+            print("- Ingrese un dato valido")
+            os.system("pause")
