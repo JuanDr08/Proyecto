db4912353fbafe7f23e3179eb42325de04c3fb05
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-03 05:42:55 -0500

    feat: :sparkles: Create camper
    Added the Crating campers part and already conected to the json file, in the future im going to catch errors

--- Modulos/Cruds/CrudCamp.py
+++ Modulos/Cruds/CrudCamp.py
@@ -1,6 +1,33 @@
 import os, json
 def create():
-    pass
+    titulo = """
+     --------------------------------------
+    |   Formulario Para Registrar Camper   |
+     --------------------------------------
+"""
+    print(titulo)
+    with open("data\Campers.json", "r") as file:
+        data = json.load(file)
+
+    edad = int(input("Cuantos años tiene el camper? -> "))
+    data.update({input("Ingrese el numero de identificacion del camper -> "):{
+        "nombre" : input("Ingrese el nombre del camper -> ").upper(),
+        "apellido" : input("Ingrese el apellido del camper -> ").upper(),
+        "direccion" : input("Ingrese la direccion del camper -> ").upper(),
+        "edad" : edad,
+        "estado": "PRE-INSCRITO",
+        "contacto" : {
+            input("- Va a ingresar un fijo o celular? -> ").upper() : int(input("Ingrese el numero de contacto -> "))
+        },
+        "acudiente" :  {
+            'nombre' : f"{input('Ingrese nombre del tutor -> ') if(edad<18) else 'NOAPLICA'}",
+            'cedula' : f"{int(input('Ingrese cedula del tutor -> ')) if(edad<18) else 'NOAPLICA'}"
+        }
+    }})
+
+    with open("data\Campers.json", "w") as file:
+        json.dump(data, file, indent = 4)
+
 def read():
     pass
 def update():
@@ -19,15 +46,19 @@ def menu():
         print(titulo)
         print(opciones)
         try:
-            opc = int(input("Ingrese la opcion que desea realizar ->"))
+            opc = int(input("Ingrese la opcion que desea realizar -> "))
             match(opc):
                 case 1:
+                    os.system("cls")
                     create()
                 case 2:
+                    os.system("cls")
                     read()
                 case 3:
+                    os.system("cls")
                     update()
                 case 4:
+                    os.system("cls")
                     delete()
                 case 5:
                     os.system("cls")
