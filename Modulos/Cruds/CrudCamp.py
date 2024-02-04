ac76d1263daf4b2ed2749627f991ace9a7139047
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-04 13:37:56 -0500

    feat: :sparkles: update system
    added the update system, there are some errors but for time im going to check them after finishing the project

--- Modulos/Cruds/CrudCamp.py
+++ Modulos/Cruds/CrudCamp.py
@@ -29,15 +29,17 @@ def create():
         json.dump(data, file, indent = 4)
 
 def read(codigo = None):
+    os.system("cls")
     with open("data/Campers.json", "r") as file:
         data = json.load(file)
     titulo = """
      ------------------------
     |   Busqueda De Campers  |
      ------------------------
-"""
-    print(titulo)
+""" 
     if(codigo == None):
+        os.system("cls")
+        print(titulo)
         for key, value in data.items():
             print(f"""
         -----------------------------
@@ -52,29 +54,52 @@ def read(codigo = None):
     """)
         os.system("pause")
     else:
-        identificacion = data.get(codigo)
-        print(f"""
+        input(f"""
         -----------------------------
         Identificacion : {codigo}
-        Nombre : {identificacion.get("nombre")}
-        Apellido : {identificacion.get("apellido")}
-        Direccion : {identificacion.get("direccion")}
-        Edad : {identificacion.get("edad")}
-        Estado : {identificacion.get("estado")}
-        Contacto {identificacion.get("contacto")}
-        Acudiente : {identificacion.get("acudiente")["nombre"]}, Cedula acudiente : {identificacion.get("acudiente")["cedula"]}
+        Nombre : {data[codigo].get("nombre")}
+        Apellido : {data[codigo].get("apellido")}
+        Direccion : {data[codigo].get("direccion")}
+        Edad : {data[codigo].get("edad")}
+        Estado : {data[codigo].get("estado")}
+        Contacto {data[codigo].get("contacto")}
+        Acudiente : {data[codigo].get("acudiente")["nombre"]}, Cedula acudiente : {data[codigo].get("acudiente")["cedula"]}
     """)
-def update():
+def update(): # add the system to change the contact and to change the accudient, check the print error
+    os.system("cls")
     while True:
         print("""
      -------------------------
     |   Actualizar un camper  |
      -------------------------
-""")
+    """)
         read()
-        tarjeta = input("Ingrese la identificacion del camper que desea eliminar -> ")
+        tarjeta = input("Ingrese la identificacion del camper que desea modificar -> ")
         read(tarjeta)
-        os.system("pause")
+        print("""
+    ¿Esta seguro que desea actualizar al camper?
+            1. Si
+            2. No
+            3.Cancelar
+    """)
+        opc = int(input("Ingrese la opcion correspondiente -> "))
+        match(opc):
+            case 1:
+                with open("data\Campers.json", "r") as file:
+                    data = json.load(file)
+                actu = input("Que desea actualizar del camper? -> ").lower()
+                if (actu == "edad"):
+                    data[tarjeta][actu] = int(input("ingrese la nueva informacion -> "))
+                else:
+                    data[tarjeta][actu] = input("ingrese la nueva informacion -> ").upper()
+                with open("data\Campers.json", "w") as file:
+                    json.dump(data, file, indent = 4)
+                os.system("cls")
+                input("Camper modificado exitosamente... ")
+                read(tarjeta)
+                break
+            case 3:
+                break
 def delete():
     pass
 def menu():
