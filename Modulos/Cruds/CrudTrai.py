0c66cc0b612acb101a8756b954926321bb08370e
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-04 14:37:16 -0500

    feat: :sparkles: Trainers crud
    started the trainers skeleton crud to structure the menu

--- Modulos/Cruds/CrudTrai.py
+++ Modulos/Cruds/CrudTrai.py
@@ -1,3 +1,5 @@
+import os, json
+
 def create():
     pass
 def read():
@@ -7,4 +9,33 @@ def update():
 def delete():
     pass
 def menu():
-    pass
+    titulo = """
+     -------------------------------------
+    |   Bienvenido Al Menu De Trainers    |
+     -------------------------------------
+"""
+    opciones = "1. Crear un trainer\n2. Buscar un trainer\n3. Modificar un trainer\n4. Borrar un trainer\n5. Volver"
+
+    while True:
+        os.system("cls")
+        print(titulo)
+        print(opciones)
+        try:
+            opc = int(input("Ingrese la opcion que desea realizar -> "))
+            match(opc):
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    os.system("cls")
+                    input("Volviendo al menu principal...")
+                case _:
+                    input("Porfavor ingrese una opcion valida")
+        except ValueError:
+            print("Porfavor ingrese un dato valido...")
+            os.system("pause")
