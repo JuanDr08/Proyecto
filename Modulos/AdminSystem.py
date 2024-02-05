9fb8a007351553593a1ba223625225214d163938
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-05 02:59:03 -0500

    feat: :sparkles: Save selection test
    i added a system to save the selection test in a json

--- Modulos/AdminSystem.py
+++ Modulos/AdminSystem.py
@@ -3,6 +3,8 @@ import os, json
 def selection():
     with open("data\Campers.json", "r") as file:
         data = json.load(file)
+    with open("data\Coordinacion.json", "r") as file:
+        dato = json.load(file)
     input("Para realizar la prueba de seleccion acontinuacion se le mostraran todos los aspirantes disponibles")
     input("Debera asignar la nota obtenida por cada alumno uno por uno (enter para continuar)")
     for key, value in data.items():
@@ -16,15 +18,25 @@ def selection():
         Apellido : {value.get("apellido")}
         Estado : {value.get("estado")}
     """)
-            if((int(input("Cual fue la nota teorica del camper? -> ")) + int(input("Cual fue la nota practica del camper? ->")))/2 >= 60):
+            nota = (int(input("Cual fue la nota teorica del camper? -> ")) + int(input("Cual fue la nota practica del camper? ->")))/2
+            if( nota >= 60):
                 value["estado"] = "INSCRITO"
                 input("El camper fue aprobado, ha logrado ingresar al equipo campus")
             else:
                 value["estado"] = "FILTRADO"
                 input("El camper no ha logrado ingresar a campus, lo sentimos")
+            dato.get("seleccion").update({key : {
+                        "hora" : input("A que hora presento la prueba el camper? -> "),
+                        "nota" : nota
+                    }                    
+                })
         else:
             os.system("cls")
             input("Actualmente no hay campers registrados con estado pre-inscrito")
+            break
+    with open("data\Coordinacion.json", "w") as file:
+        json.dump(dato, file, indent = 4)
+        file.close()
     with open("data\Campers.json", "w") as file:
         json.dump(data, file, indent=4)
         file.close()
