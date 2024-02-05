dd89a0d88f402437477e125886bdf1d73f0faca6
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-05 01:01:43 -0500

    feat: :sparkles: Coordination menu
    Created the menu and conected the options that will be able to do in the menu, called the  functions in the other file where im going to do all the operations

---
+++ Modulos/AdminSystem.py
@@ -0,0 +1,12 @@
+import os, json
+
+def selection():
+    pass
+def pruebas():
+    pass
+def ruta():
+    pass
+def rooms():
+    pass
+def giveroom():
+    pass
--- Modulos/ui/Menu.py
+++ Modulos/ui/Menu.py
@@ -1,8 +1,44 @@
 import Modulos.Cruds.CrudCamp as camp
 import Modulos.Cruds.CrudTrai as trai
+import Modulos.AdminSystem as process
 import os
 def menuCoo():
-    pass
+    while True:
+        os.system("cls")
+        print(f"""
+    ********************************
+    *    Coordinacion Academica    *
+    ********************************
+""")
+        print(f"1. Prueba de admision\n2. Examenes modulo\n3. Crear rutas\n4. Crear salas\n5. Asignar salones\n6. Volver")
+        try:
+            opc = int(input("Ingrese la opcion que desea  realizar -> "))
+            match(opc):
+                case 1:
+                    os.system("cls")
+                    process.selection()
+                case 2:
+                    os.system("cls")
+                    process.pruebas()
+                case 3:
+                    os.system("cls")
+                    process.ruta()
+                case 4:
+                    os.system("cls")
+                    process.rooms()
+                case 5:
+                    os.system("cls")
+                    process.giveroom()
+                case 6:
+                    os.system("cls")
+                    input("Volviendo...")
+                    break
+                case _:
+                    os.system("cls")
+                    input("Ingrese una opcion disponble...")
+        except ValueError:
+            os.system("cls")
+            input("Ingrese un dato valido...")
 def report():
     pass
 def menuPrincipal():
