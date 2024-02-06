99f29bdc0578fc1e10378d0d59b04039ab0e524b
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-06 01:33:42 -0500

    fix: :bug: Find the existance of campers
    only added a condition to first check if there is a camper created to continue with the process of the selection test

--- Modulos/AdminSystem.py
+++ Modulos/AdminSystem.py
@@ -5,35 +5,39 @@ def selection():
         data = json.load(file)
     with open("data\Coordinacion.json", "r") as file:
         dato = json.load(file)
-    input("Para realizar la prueba de seleccion acontinuacion se le mostraran todos los aspirantes disponibles")
-    input("Debera asignar la nota obtenida por cada alumno uno por uno (enter para continuar)")
-    for key, value in data.items():
-        os.system("cls")
-        if (value.get("estado")== "PRE-INSCRITO"):
-            print(f"""
-            
-        -----------------------------
-        Identificacion : {key}
-        Nombre : {value.get("nombre")}
-        Apellido : {value.get("apellido")}
-        Estado : {value.get("estado")}
-    """)
-            nota = (int(input("Cual fue la nota teorica del camper? -> ")) + int(input("Cual fue la nota practica del camper? ->")))/2
-            if( nota >= 60):
-                value["estado"] = "INSCRITO"
-                input("El camper fue aprobado, ha logrado ingresar al equipo campus")
-            else:
-                value["estado"] = "FILTRADO"
-                input("El camper no ha logrado ingresar a campus, lo sentimos")
-            dato.get("seleccion").update({key : {
-                        "hora" : input("A que hora presento la prueba el camper? -> "),
-                        "nota" : nota
-                    }                    
-                })
-        else:
+    if(len(data) >=1):
+        input("Para realizar la prueba de seleccion acontinuacion se le mostraran todos los aspirantes disponibles")
+        input("Debera asignar la nota obtenida por cada alumno uno por uno (enter para continuar)")
+        for key, value in data.items():
             os.system("cls")
-            input("Actualmente no hay campers registrados con estado pre-inscrito")
-            break
+            if (value.get("estado")== "PRE-INSCRITO"):
+                print(f"""
+                
+            -----------------------------
+            Identificacion : {key}
+            Nombre : {value.get("nombre")}
+            Apellido : {value.get("apellido")}
+            Estado : {value.get("estado")}
+        """)
+                nota = (int(input("Cual fue la nota teorica del camper? -> ")) + int(input("Cual fue la nota practica del camper? ->")))/2
+                if( nota >= 60):
+                    value["estado"] = "INSCRITO"
+                    input("El camper fue aprobado, ha logrado ingresar al equipo campus")
+                else:
+                    value["estado"] = "FILTRADO"
+                    input("El camper no ha logrado ingresar a campus, lo sentimos")
+                dato.get("seleccion").update({key : {
+                            "hora" : input("A que hora presento la prueba el camper? -> "),
+                            "nota" : nota
+                        }                    
+                    })
+            else:
+                os.system("cls")
+                input("Actualmente no hay campers registrados con estado pre-inscrito")
+                break
+    else:
+        os.system("cls")
+        input("No hay ningun camper registrado")
     with open("data\Coordinacion.json", "w") as file:
         json.dump(dato, file, indent = 4)
         file.close()
@@ -91,7 +95,7 @@ def rooms():
         if (sala not in data.get("rooms")):
             data.get("rooms").update({sala : {
                 "capacidad" : int(input("Ingrese la capacidad maxima de la sala -> ")),
-                "estado" : "VACIO"
+                "estado" : {}
             }})
             with open("data\Coordinacion.json", "w") as file:
                 json.dump(data, file, indent = 4)
