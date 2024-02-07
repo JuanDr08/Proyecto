28ab95c7aebfa7d3c95ff0c488bbbb80c3a437f0
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-02-07 12:05:56 -0500

    chore: :lipstick: added a arrow
    in the date of start and end
    
    @CampusLands

--- Modulos/AdminSystem.py
+++ Modulos/AdminSystem.py
@@ -190,8 +190,8 @@ def giveroom():
                                         })
                                         admin["classrooms"][code].update({
                                             sala : hora,
-                                            "inicio" : input("Ingrese la fecha de inicio de entrenamiento de este grupo "),
-                                            "fin" : input("Ingrese la fecha de finalizacion de entrenamiento de este grupo")
+                                            "inicio" : input("Ingrese la fecha de inicio de entrenamiento de este grupo -> "),
+                                            "fin" : input("Ingrese la fecha de finalizacion de entrenamiento de este grupo -> ")
                                         })    
                                         trai[trainer].update({code : {"sala" : sala}}) 
                                         input("Trainer, Hora y sala asignada correctamente")
